cc4a6e8c6fbe0869d26df8c9cf29e06de3c6c434
T107 <T107@PC247>
2014-06-02 19:43:34 -0500

    Gabino Meneses

---
+++ src/gabino/angelica/ProbarPrimitivos.java
@@ -0,0 +1,13 @@
+
+
+package gabino.angelica;
+
+public class ProbarPrimitivos {
+    public static void main(String args[]){
+        byte b=2;
+        short s=2;
+        int i=2;
+        long l=2;
+    }
+    
+}
